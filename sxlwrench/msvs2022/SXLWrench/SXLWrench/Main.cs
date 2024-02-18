e432671f9a5bc0cd91602d8cfa450fa963331ab2
SG-Mods <91380903+SG-Mods@users.noreply.github.com>
2024-02-18 02:37:31 -0500

    Made Changes
    Changes made to reflect moving code to single .dll with an entry point tbd but utilizing UMM

---
+++ sxlwrench/msvs2022/SXLWrench/SXLWrench/Main.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+using UnityModManagerNet;
+
+/* Notes
+ * added references for the following
+ * SkaterXL.Core
+ * 0Harmony
+ * Assembly-CSharp
+ * Assembly-CSharp-firstpass FROM MANAGED
+ * UnityEngine
+ * UnityEngine.UI
+ * UnityEngine.CoreModule
+ * UnityEngine.IMGUIModule
+ * UnityModManager
+ * */
+
+namespace SXLWrenchDLLm
+{
+    static class Main
+    {
+        static void Load()
+        {
+            SkaterXL.Core.SXLWrench.PushDataLocal("From DLL", true);
+            //harmony
+        }
+    }
+}
